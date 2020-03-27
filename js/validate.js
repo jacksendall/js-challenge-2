b4417e92de9895fbfe1273aa146c761299747436
jacksendall <jacksendall@gmail.com>
2020-03-27 16:31:41 +0000

    verson 1

--- js/validate.js
+++ js/validate.js
@@ -1,20 +1,23 @@
 const input = document.querySelector('#email-input');
+const field = document.querySelector('#form-field');
 
 function checkForm(form)
   {
     // validation fails if the input is blank
     if(input.value == "") {
-      alert("Error: Input is empty!");
+      field.classList.add('empty-invalid');
       form.inputfield.focus();
+
       return false;
     }
 
     // regular expression to match only email addresses.
     var re = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
 
-    // validation fails if the input doesn't match our regular expression
+    // validation fails if the input doesn't match regular expression
     if(!re.test(input.value)) {
-      alert("Error: Input contains invalid characters!");
+    //   alert("Error: Input contains invalid characters!");
+      field.classList.add('email-invalid');
       input.focus();
       return false;
     }
@@ -23,8 +26,52 @@ function checkForm(form)
     return true;
   }
 
-  document.getElementById("submitBtn").addEventListener("click", function(event){
-    event.preventDefault();
 
+
+//Images==============
+
+let activeImg = ``;
+
+function searchImages(){
+    var clientId = '7mLGkdcJUsLvwq_PO2_pa3s_nkXqiQhac32gYI_0RKs';
+    var query =  document.querySelector('#query-input').value;
+    var url = 'https://api.unsplash.com/photos/random/?client_id='+clientId+'&query='+query;
+
+    fetch(url)
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function(data) {
+            let result = `<img src="${data.urls.regular}">`;
+            activeImg = `${data.urls.regular}`;
+            $('.photo-frame').html(result);
+        });
+
+    
+
+}
+//save active image
+function saveImage(){
+    console.log("saved:"+activeImg);
+}
+
+//define Buttons =============
+
+$('#form-field').submit(function(event){
+    event.preventDefault();
+    // clear 'invalid' style from form element
+    field.className = "";
+
+    // run the function
     checkForm();
-  });
+    if(checkForm){
+        //Save the active image
+        saveImage();
+    }
+});
+
+$('#searchField').submit(function(event){
+    event.preventDefault();
+    searchImages();
+})
+
